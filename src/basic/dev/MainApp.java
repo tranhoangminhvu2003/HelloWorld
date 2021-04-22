d5f65087a9302b1b6717f613335e4eef80766d0a
tranhoangminhvu2003 <tranhoangminhvu2002@example.com>
2021-04-22 15:21:52 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,15 @@
+package basic.dev;
+/**
+ * lab03: HelloWorld Hello world
+ * @author TGDD
+ *
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+           System.out.println("Hello World");
+
+}
+}
